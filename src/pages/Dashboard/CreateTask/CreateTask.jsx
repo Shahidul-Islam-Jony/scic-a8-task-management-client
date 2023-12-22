22392c8717811fa5032527c8b5e5c0d243ab3616
Shahidul-Islam-Jony <shahidulislam13sf@gmail.com>
2023-12-22 11:21:20 -0800

    Tasks created and stored to mongodb

--- src/pages/Dashboard/CreateTask/CreateTask.jsx
+++ src/pages/Dashboard/CreateTask/CreateTask.jsx
@@ -1,11 +1,31 @@
+import { useContext } from "react";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { useForm } from "react-hook-form";
+import { AuthContext } from "../../../providers/AuthProvider";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
 
 
 const CreateTask = () => {
+    const { user } = useContext(AuthContext);
+    console.log(user);
+    const axiosPublic = useAxiosPublic();
     const { register, handleSubmit, formState: { errors }, } = useForm();
     const onSubmit = (data) => {
         console.log(data);
+        const tasks = {
+            email: user.email,
+            title: data.title,
+            deadline: data.deadline,
+            description: data.description,
+            priority: data.priority,
+            type: 'todo'
+        }
+        console.log(tasks);
+        axiosPublic.post('/addTasks', tasks)
+            .then(res => {
+                console.log(res);
+            })
+
     }
     return (
         <HelmetProvider>
@@ -15,19 +35,19 @@ const CreateTask = () => {
                     <div className='w-full mx-auto'>
                         <h1 className="text-5xl font-bold mb-4">Create New Task!</h1>
                         <div className="rounded-lg w-full shadow-2xl bg-base-100 px-4 py-6">
-                            <form  onSubmit={handleSubmit(onSubmit)}>
+                            <form onSubmit={handleSubmit(onSubmit)}>
                                 <div>
                                     <label className="label">
                                         <span className="text-xl font-medium">Title</span>
                                     </label>
-                                    <input type="text" {...register("title",{ required: true })} placeholder="title" className="input rounded-md w-full border-pink-600" />
+                                    <input type="text" {...register("title", { required: true })} placeholder="title" className="input rounded-md w-full border-pink-600" />
                                     {errors.title && <span className="text-red-500">Title is required</span>}
                                 </div>
                                 <div>
                                     <label className="label">
                                         <span className="text-xl font-medium">Deadline</span>
                                     </label>
-                                    <input type="date" {...register("deadline",{ required: true })} className="input rounded-md w-full border-pink-600" />
+                                    <input type="date" {...register("deadline", { required: true })} className="input rounded-md w-full border-pink-600" />
                                     {errors.deadline && <span className="text-red-500">Deadline is required</span>}
                                 </div>
                                 {/* user type */}
@@ -35,7 +55,7 @@ const CreateTask = () => {
                                     <label className="label">
                                         <span className="text-xl font-medium">Priority</span>
                                     </label>
-                                    <select {...register("priority",{ required: true })} className="select select-bordered text-lg border-pink-600">
+                                    <select {...register("priority", { required: true })} className="select select-bordered text-lg border-pink-600">
                                         <option disabled selected>Please select task priority</option>
                                         <option value='low'>Low</option>
                                         <option value='moderate'>Moderate</option>
